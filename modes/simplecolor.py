e98a59cba071c714cd6b7d2829d99a7bd99522c4
Aaron Shoemaker <aaronshoemaker@gmail.com>
2018-08-22 21:49:01 -0700

    Cleaned up simplecolor implementation.

--- modes/simplecolor.py
+++ modes/simplecolor.py
@@ -5,8 +5,6 @@ class SimpleColor:
     pixelmapper = None
     pixelcontroller = None
     tick_ms = None
-    axis_state = None
-    pressed_buttons = None
 
     def __init__(self, pixelmapper, pixelcontroller, tick_ms=100):
         self.pixelmapper = pixelmapper
@@ -18,30 +16,15 @@ class SimpleColor:
 
         # If the base color is currently off, give it an initial value
         if sum(self.pixelmapper.base_color) == 0:
-            self.pixelmapper.base_color = (96, 0, 0)
-
-        for s in self.pixelmapper.strands:
-            s.leds = [self.pixelmapper.base_color] * s.length
-        self.pixelcontroller.set_pixel_map(self.pixelmapper)
+            self.pixelmapper.base_color = self.pixelmapper.get_random_color(min_brightness=32)
 
     def stop(self):
-        for s in self.pixelmapper.strands:
-            s.leds = [COLOR_BLANK] * s.length
-        self.pixelcontroller.set_pixel_map(self.pixelmapper)
+        self.pixelcontroller.set_color()
 
         print("%s stopped" % __name__)
-        pass
 
     def input(self, event, axis, buttons):
         pass
 
     def tick(self):
-        if self.pixelmapper.base_color_changed:
-            self.refresh_color()
-
-    def refresh_color(self):
-        for s in self.pixelmapper.strands:
-            s.leds = [self.pixelmapper.base_color] * s.length
-
-        self.pixelcontroller.set_pixel_map(self.pixelmapper)
-        self.pixelmapper.base_color_changed = False
+        self.pixelcontroller.set_color(self.pixelmapper.base_color)
